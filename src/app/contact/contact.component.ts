8e775de7df319d9d86393220e46a608becad9745
xfrends <xfrends@snapinn.id>
2019-05-09 09:37:27 +0700

    edit respondsive & add emailjs (not working)

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -10,8 +10,24 @@ export class ContactComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    
+    var email 	= require("./path/to/emailjs/email");
+    var server 	= email.server.connect({
+      user:    "frenditriarista@s4codingcenter.com", 
+      password:"password1207",
+      host:    "mail.s4codingcenter.com",
+      ssl:     true
+    });
+  }
 
+  sendEmail(form) {
+    // send the message and get a callback with an error or details of the message that was sent
+    server.send({
+      text:    "i hope this works", 
+      from:    "frendi1207@gmail.com.com", 
+      to:      "frenditriarista@s4codingcenter.com",
+      cc:      "xfrends@snapinn.id",
+      subject: "testing emailjs"
+    }, function(err, message) { console.log(err || message); });
   }
   
 }
